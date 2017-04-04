758f2aabd35b2a8b8c3bee1b4740d6287ffb2d07
Jack Dinkel <johnpauldinkel@gmail.com>
2017-04-03 18:42:31 -0600

    tested decoder

--- mask.py
+++ mask.py
@@ -8,7 +8,7 @@ def Get_Bits_31_26(number):
 
 def Get_Bits_25_21(number):
   assert number >= 0x0 and number <= 0xFFFFFFFF
-  mask = 0x03600000
+  mask = 0x03E00000
   return (number & mask) >> 21
 
 def Get_Bits_20_16(number):
@@ -21,13 +21,22 @@ def Get_Bits_15_11(number):
   mask = 0x0000F800
   return (number & mask) >> 11
 
+def Get_Bits_10_6(number):
+  assert number >= 0x0 and number <= 0xFFFFFFFF
+  mask = 0x000007C0
+  return (number & mask) >> 6
+
+def Get_Bits_5_0(number):
+  assert number >= 0x0 and number <= 0xFFFFFFFF
+  mask = 0x0000002F
+  return (number & mask)
+
 def Get_Bits_15_0(number):
   assert number >= 0x0 and number <= 0xFFFFFFFF
   mask = 0x0000FFFF
   return (number & mask)
 
-def Get_Bits_5_0(number):
+def Get_Bits_25_0(number):
   assert number >= 0x0 and number <= 0xFFFFFFFF
-  mask = 0x0000001F
+  mask = 0x03FFFFFF
   return (number & mask)
-
--- unit_test.py
+++ unit_test.py
@@ -1,47 +1,102 @@
-from hardware import *
+import hardware as HW
+import decode
 import pytest
 
 
 def test_mux():
-  assert MUX("hi", 123, 0) == "hi"
-  assert MUX("hi", 123, 1) == 123
+  assert HW.MUX("hi", 123, 0) == "hi"
+  assert HW.MUX("hi", 123, 1) == 123
+
+
 
 def test_PC():
   v = 0
-  mypc = PC(v)
+  mypc = HW.PC(v)
   assert mypc.Get() == v
 
   v = 400
   mypc.Update(v)
   assert mypc.Get() == v
 
+
 @pytest.mark.skip()
 def test_Instruction_Memory():
   #TODO
   pass
 
+
+
+def test_Decode():
+  decoder = decode.Decoder()
+
+  instr = 0x00000000
+  decoder.decode(instr)
+  assert decoder.op == 0x0
+  assert decoder.rs == 0x0
+  assert decoder.rt == 0x0
+  assert decoder.rd == 0x0
+  assert decoder.shamt == 0x0
+  assert decoder.funct == 0x0
+  assert decoder.i_imm == 0x0
+  assert decoder.j_imm == 0x0
+
+  # subu t4,t5,t3
+  instr = 0x01ab6023 
+  decoder.decode(instr)
+  assert decoder.op == 0x0
+  assert decoder.rs == 0xD
+  assert decoder.rt == 0xB
+  assert decoder.rd == 0xC
+  assert decoder.shamt == 0x0
+  assert decoder.funct == 0x23
+
+  # addiu t5,a1,-1
+  instr = 0x24adffff 
+  decoder.decode(instr)
+  assert decoder.op == 0x9
+  assert decoder.rs == 0x5
+  assert decoder.rt == 0xD
+  assert decoder.i_imm == 0xFFFF
+
+  # jal 120
+  instr = 0x0c000078 
+  decoder.decode(instr)
+  assert decoder.op == 0x3
+  assert decoder.j_imm == 0x78
+
+
 def test_Add_Four():
   v = 12
-  assert Add_Four(v) == v + 4
+  assert HW.Add_Four(v) == v + 4
+
+
 
 @pytest.mark.skip()
 def test_Register_File():
   #TODO
   pass
 
+
+
 @pytest.mark.skip()
 def test_Sign_Extend():
   #TODO
   pass
+
+
 
 @pytest.mark.skip()
 def test_ALU():
   #TODO
   pass
 
+
+
 def test_Shift_Left_2():
   v = 154
-  assert Shift_Left_2(v) == v << 2
+  assert HW.Shift_Left_2(v) == v << 2
+
+
 
 @pytest.mark.skip()
 def test_Data_Memory():
